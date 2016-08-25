cf64696aa2a47e11aa9ee32de037f3da633d03cd
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-08-25 16:06:53 -0400

    Adjust paths so that meta.yaml is corrected properly

--- .meta_yaml_replacer.py
+++ .meta_yaml_replacer.py
@@ -3,14 +3,14 @@
 # Copyright (c) 2016, Michael Boyle
 # See LICENSE file for details: <https://github.com/moble/quaternion/blob/master/LICENSE>
 
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 
 import fileinput
 from auto_version import calculate_version
 
-version_string = calculate_version().replace('+dirty', '')
+version_string = calculate_version().replace('.dirty', '')
 
-f = fileinput.FileInput('meta.yaml', inplace=True)
+f = fileinput.FileInput('conda/meta.yaml', inplace=True)
 for line in f:
     print(line.replace("version: '1.0'", "version: '{0}'".format(version_string)), end='')
 f.close()
