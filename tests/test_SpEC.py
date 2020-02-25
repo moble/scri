fc4ec3f723e5bbac7151039fefbb98fc67b41b67
10220@Aule <danteiozzo.gmail.com>
2020-02-25 14:56:40 -0500

    Add extrapolation tests to test_SpEC.py

--- tests/test_SpEC.py
+++ tests/test_SpEC.py
@@ -9,28 +9,90 @@ import numpy as np
 import h5py
 import scri
 import scri.SpEC
+import shutil
 
 
-@pytest.mark.skipif(not os.path.exists('../SpEC/samples/rhOverM_Asymptotic_GeometricUnits.h5'),
-                    reason="requires SpEC/samples/*.h5")
+# NOTE: test_file_io() depends on output from test_NRAR_extrapolation(), so the latter
+# must come first in this file.
+
+filename = "rh_FiniteRadii_CodeUnits.h5"
+input_dir = "../SpEC/samples/"
+output_dir_base = "../SpEC/samples/test_output/"
+
+
+@pytest.mark.skipif(
+    not os.path.exists(input_dir + filename),
+    reason="requires {}".format(input_dir + filename),
+)
+def test_extrapolation():
+    """
+    Test extrapolation of waveforms with output in the scri format.
+    """
+    output_dir = output_dir_base + "test_extrapolation_output/"
+    # Extrapolation fails if the output filenames exist so we delete
+    # the output files from a previous test if they exist.
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
+    scri.extrapolation.extrapolate(
+        InputDirectory=input_dir, OutputDirectory=output_dir, ChMass=1.0
+    )
+
+
+@pytest.mark.skipif(
+    not os.path.exists(input_dir + filename),
+    reason="requires {}".format(input_dir + filename),
+)
+def test_NRAR_extrapolation():
+    """
+    Test extrapolation of waveforms with output in the NRAR format.
+    """
+    output_dir = output_dir_base + "test_NRAR_extrapolation_output/"
+    # Extrapolation fails if the output filenames exist so we delete
+    # the output files from a previous test if they exist.
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
+    # Computing DifferenceFiles and making plots are tested in test_extrapolation() so
+    # to save time, we don't do it here.
+    scri.extrapolation.extrapolate(
+        InputDirectory=input_dir,
+        OutputDirectory=output_dir,
+        DifferenceFiles="",
+        PlotFormat="",
+        UseStupidNRARFormat=True,
+        ChMass=1.0,
+    )
+
+
+test_file = "../SpEC/samples/test_output/test_NRAR_extrapolation_output/rhOverM_Extrapolated_N2.h5"
+
+@pytest.mark.skipif(
+    not os.path.exists(test_file), reason="requires {}".format(test_file)
+)
 def test_file_io():
     """Test file I/O with a round-trip and compare H5 files"""
-    w = scri.SpEC.read_from_h5('../SpEC/samples/rhOverM_Asymptotic_GeometricUnits.h5/Extrapolated_N2.dir')
-    scri.SpEC.write_to_h5(w, '../SpEC/samples/Asymptotic_GeometricUnits_test.h5/Extrapolated_N2.dir')
+    w = scri.SpEC.read_from_h5(test_file)
+    scri.SpEC.write_to_h5(w, "../SpEC/samples/Asymptotic_GeometricUnits_test.h5")
 
-    f1 = h5py.File('../SpEC/samples/rhOverM_Asymptotic_GeometricUnits.h5')
-    f2 = h5py.File('../SpEC/samples/rhOverM_Asymptotic_GeometricUnits_test.h5')
-    w1 = f1['Extrapolated_N2.dir']
-    w2 = f2['Extrapolated_N2.dir']
+    f1 = h5py.File(test_file,'r')
+    f2 = h5py.File("../SpEC/samples/rhOverM_Asymptotic_GeometricUnits_test.h5",'r')
 
     # Check top-level attributes
-    assert list(w1.attrs.items())[1:] == list(w2.attrs.items())[1:]  # 0 attribute is waveform output version
+    list1 = sorted(f1.attrs.items())
+    list2 = sorted(f2.attrs.items())
+    # attribute 3 is waveform output version
+    list1.remove(list1[3])
+    list2.remove(list2[3])
+    assert list1 == list2
 
     # Check that original history is contained (with extra comment characters) in second history
-    assert ('# ' + '\n# '.join(w1['History.txt'][()].decode().split('\n'))) in w2['History.txt'][()]
+    assert ("# " + "\n# ".join(f1["History.txt"][()].split("\n"))) in f2[
+        "History.txt"
+    ][()]
 
     # Now check each mode from the original
-    for mode in list(w1):
-        if mode.startswith('Y'):
-            assert list(w1[mode].attrs.items()) == list(w2[mode].attrs.items())
-            assert np.array_equal(w1[mode][:], w2[mode][:])
+    for mode in list(f1):
+        if mode.startswith("Y"):
+            assert list(f1[mode].attrs.items()) == list(f2[mode].attrs.items())
+            assert np.array_equal(f1[mode][:], f2[mode][:])
