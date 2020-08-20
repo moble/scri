8e8b8dbb15e649a57b46ee33aaebe7ab9345a30c
keefemitman <kmitman@caltech.edu>
2020-08-20 09:52:19 -0500

    adding updated files after accidental deletion

--- scri/SpEC/memory.py
+++ scri/SpEC/memory.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-
 import numpy as np
 import spherical_functions as sf
 from spherical_functions import LM_index as lm
 from ..modes_time_series import ModesTimeSeries
 
 
-def WMtoMTS(h_wm):
+def create_ModesTimeSeries_from_WaveformModes(h_wm):
     h_mts = ModesTimeSeries(
         sf.SWSH_modes.Modes(
             h_wm.data,
@@ -21,6 +19,10 @@ def WMtoMTS(h_wm):
 
 
 def D_Op(h_mts, UseLaplacian=False):
+    """ Differential operator as defined in
+    equation () of arXiv: (update when paper is finished)
+    """
+    
     h = h_mts.copy()
     s = h_mts.view(np.ndarray)
 
@@ -40,7 +42,7 @@ def D_Op(h_mts, UseLaplacian=False):
     return h
 
 
-def mem_mass_aspect(h, Psi2, news=None):
+def mem_mass_aspect_contribution(h, Psi2, news=None):
     """Calculate the Bondi mass aspect contribution to the 
     electric part of the strain according to 
     equation () of arXiv: (update when paper is finished)
@@ -51,7 +53,7 @@ def mem_mass_aspect(h, Psi2, news=None):
         WaveformModes object corresponding to the strain
     Psi2 : WaveformModes
         WaveformModes object corresponding to Psi2
-    news : WaveformModes [default is None]
+    news : WaveformModes [default is h.data_dot]
         WaveformModes object corresponding to the news;
         if the default is None, then compute this as the
         time-derivative of the strain
@@ -66,13 +68,13 @@ def mem_mass_aspect(h, Psi2, news=None):
     if news == None:
         news = h.copy()
         news.data = h.data_dot
-    h_mts = WMtoMTS(h)
-    Psi2_mts = WMtoMTS(Psi2)
-    news_mts = WMtoMTS(news)
+    h_mts = create_ModesTimeSeries_from_WaveformModes(h)
+    Psi2_mts = create_ModesTimeSeries_from_WaveformModes(Psi2)
+    news_mts = create_ModesTimeSeries_from_WaveformModes(news)
 
     Bondi_M_mts = h_mts.copy()
     Bondi_M_mts = (
-        0.5 * D_Op(-(Psi2_mts + 0.25 * 1.0j * h_mts.eth.eth.imag * -1.0j + 0.25 * (news_mts * h_mts.bar))).ethbar.ethbar
+        0.5 * D_Op(-(Psi2_mts + 0.25 * 1.0j * h_mts.eth.eth.imag + 0.25 * (news_mts * h_mts.bar))).ethbar.ethbar
     )
     # "* -1.0j" since we want the imaginary part, not the imaginary component
 
@@ -82,7 +84,7 @@ def mem_mass_aspect(h, Psi2, news=None):
     return Bondi_M
 
 
-def mem_energy_flux(h, news=None):
+def mem_energy_flux_contribution(h, news=None, start_time=None):
     """Calculate the energy flux contribution to the 
     electric part of the strain according to 
     equation () of arXiv: (update when paper is finished)
@@ -91,10 +93,14 @@ def mem_energy_flux(h, news=None):
     ----------
     h : WaveformModes
         WaveformModes object corresponding to the strain
-    news : WaveformModes [default is None]
+    news : WaveformModes [default is h.data_dot]
         WaveformModes object corresponding to the news;
         if the default is None, then compute this as the
         time-derivative of the strain
+    start_time : double [default is h.t[0]]
+        double corresponding to the time at which the
+        energy flux integral should begin
+
 
     Returns
     -------
@@ -106,11 +112,16 @@ def mem_energy_flux(h, news=None):
     if news == None:
         news = h.copy()
         news.data = h.data_dot
-    news_mts = WMtoMTS(news)
+    news_mts = create_ModesTimeSeries_from_WaveformModes(news)
+
+    
 
     E_flux_mts = news_mts.copy()
     E_flux_mts = 0.25 * (news_mts * news_mts.bar).int
     E_flux_mts = 0.5 * D_Op(E_flux_mts).ethbar.ethbar
+
+    start_time_idx = np.argmin(abs(h.t - start_time))
+    E_flux_mts = E_flux_mts - E_flux_mts[start_time_idx,:]
 
     E_flux = h.copy()
     E_flux.data = np.array(E_flux_mts[:, lm(2, -2, E_flux_mts.ell_min) :])
@@ -118,7 +129,7 @@ def mem_energy_flux(h, news=None):
     return E_flux
 
 
-def mem_angular_momentum_aspect(h, Psi1, news=None):
+def mem_angular_momentum_aspect_contribution(h, Psi1, news=None):
     """Calculate the angular momentum aspect contribution to the 
     magnetic part of the strain according to
     equation () of arXiv: (update when paper is finished)
@@ -129,7 +140,7 @@ def mem_angular_momentum_aspect(h, Psi1, news=None):
         WaveformModes object corresponding to the strain
     Psi1 : WaveformModes
         WaveformModes object corresponding to Psi1
-    news : WaveformModes [default is None]
+    news : WaveformModes [default is h.data_dot]
         WaveformModes object corresponding to the news;
         if the default is None, then compute this as the
         time-derivative of the strain
@@ -144,16 +155,16 @@ def mem_angular_momentum_aspect(h, Psi1, news=None):
     if news == None:
         news = h.copy()
         news.data = h.data_dot
-    h_mts = WMtoMTS(h)
-    Psi1_mts = WMtoMTS(Psi1)
-    news_mts = WMtoMTS(news)
+    h_mts = create_ModesTimeSeries_from_WaveformModes(h)
+    Psi1_mts = create_ModesTimeSeries_from_WaveformModes(Psi1)
+    news_mts = create_ModesTimeSeries_from_WaveformModes(news)
 
     Bondi_Ndot_mts = h_mts.copy()
     Bondi_Ndot_mts = (
         0.5
         * 1.0j
         * D_Op(
-            (2.0 * Psi1_mts - 0.25 * (h_mts.bar * h_mts.eth)).ethbar.dot.imag * -1.0j, UseLaplacian=True
+            (2.0 * Psi1_mts - 0.25 * (h_mts.bar * h_mts.eth)).ethbar.dot.imag, UseLaplacian=True
         ).ethbar.ethbar
     )
     # "* -1.0j" since we want the imaginary part, not the imaginary component
@@ -164,7 +175,7 @@ def mem_angular_momentum_aspect(h, Psi1, news=None):
     return Bondi_Ndot
 
 
-def mem_angular_momentum_flux(h, news=None):
+def mem_angular_momentum_flux_contribution(h, news=None):
     """Calculate the angular momentum flux contribution to the 
     magnetic part of the strain according to 
     equation () of arXiv: (update when paper is finished)
@@ -173,7 +184,7 @@ def mem_angular_momentum_flux(h, news=None):
     ----------
     h : WaveformModes
         WaveformModes object corresponding to the strain
-    news : WaveformModes [default is None]
+    news : WaveformModes [default is h.data_dot]
         WaveformModes object corresponding to the news;
         if the default is None, then compute this as the
         time-derivative of the strain
@@ -188,8 +199,8 @@ def mem_angular_momentum_flux(h, news=None):
     if news == None:
         news = h.copy()
         news.data = h.data_dot
-    h_mts = WMtoMTS(h)
-    news_mts = WMtoMTS(news)
+    h_mts = create_ModesTimeSeries_from_WaveformModes(h)
+    news_mts = create_ModesTimeSeries_from_WaveformModes(news)
 
     Jdot_flux_mts = h_mts.copy()
     Jdot_flux_mts = (
@@ -202,8 +213,7 @@ def mem_angular_momentum_flux(h, news=None):
                 - 3.0 * news_mts * h_mts.bar.ethbar
                 + news_mts.bar * h_mts.ethbar
                 - h_mts.bar * news_mts.ethbar
-            ).eth.imag
-            * -1.0j,
+            ).eth.imag,
             UseLaplacian=True,
         ).ethbar.ethbar
     )
@@ -215,7 +225,7 @@ def mem_angular_momentum_flux(h, news=None):
     return Jdot_flux
 
 
-def Add_Memory(h):
+def add_memory(h, start_time=None):
     """Calculate the strain with the electric component of
     the null memory added on, i.e., the contribution from the energy flux
 
@@ -223,6 +233,9 @@ def Add_Memory(h):
     ----------
     h : WaveformModes
         WaveformModes object corresponding to the strain
+    start_time : double [default is h.t[0]]
+        double corresponding to the time at which the
+        energy flux integral should begin
 
     Returns
     -------
@@ -232,12 +245,12 @@ def Add_Memory(h):
     """
 
     h_with_mem = h.copy()
-    h_with_mem.data = h.data + mem_energy_flux(h).data
+    h_with_mem.data = h.data + mem_energy_flux_contribution(h, start_time=start_time).data
 
     return h_with_mem
 
 
-def BMS_strain(h, Psi2, Psi1, news=None):
+def BMS_strain(h, Psi2, Psi1, news=None, start_time=None, match_time=None):
     """Calculate what the strain should be according to the BMS balance laws, i.e.,
     equations () of arXiv: (update when paper is finished)
 
@@ -245,21 +258,43 @@ def BMS_strain(h, Psi2, Psi1, news=None):
     ----------
     h : WaveformModes
         WaveformModes object corresponding to the strain
+    Psi2 : WaveformModes
+        WaveformModes object corresponding to Psi2
+    Psi1 : WaveformModes
+        WaveformModes object corresponding to Psi1
+    news : WaveformModes [default is h.data_dot]
+        WaveformModes object corresponding to the news;
+        if the default is None, then compute this as the
+        time-derivative of the strain
+    start_time : double [default is h.t[0]]
+        double corresponding to the time at which the
+        energy flux integral should begin
+    match_time : double [default is None]
+        double corresponding to the time at which the
+        strain and BMS strain should match
 
     Returns
     -------
-    (h_BMS, M, E, Ndot, Jdot) : ntuple of WaveformModes
+    (h_BMS, Constraint, M, E, Ndot, Jdot) : ntuple of WaveformModes
         ntuple of WaveformModes objects corresponding to the strain from the BMS balance laws,
+        the BMS constraint, i.e., equation () of arXiv: (update when paper is finished),
         and the contributions from the Bondi mass aspect, the energy flux,
         the Bondi angular momentum aspect, and the angular momentum flux
 
     """
 
     h_BMS = h.copy()
-    M = mem_mass_aspect(h, Psi2, news)
-    E = mem_energy_flux(h, news)
-    Ndot = mem_angular_momentum_aspect(h, Psi1, news)
-    Jdot = mem_angular_momentum_flux(h, news)
+    M = mem_mass_aspect_contribution(h, Psi2, news)
+    E = mem_energy_flux_contribution(h, news=news, start_time=start_time)
+    Ndot = mem_angular_momentum_aspect_contribution(h, Psi1, news)
+    Jdot = mem_angular_momentum_flux_contribution(h, news)
     h_BMS.data = M.data + E.data + Ndot.data + Jdot.data
 
-    return (h_BMS, M, E, Ndot, Jdot)
+    if not match_time == None:
+        match_time_idx = np.argmin(abs(h.t-match_time))
+        h_BMS.data = h_BMS.data + (h.data - h_BMS.data)[match_time_idx,:]
+
+    Constraint = h.copy()
+    Constraint.data = h.data - h_BMS.data
+    
+    return (h_BMS, Constraint, M, E, Ndot, Jdot)
