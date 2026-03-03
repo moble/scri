ec5545e0577fc362c5f7357c82e0ea0f935e1706
Mike Boyle <michael.oliver.boyle@gmail.com>
2026-03-03 14:34:41 -0500

    Apply some formatting

--- scri/pn/boosted_comcharge.py
+++ scri/pn/boosted_comcharge.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def PN_charges(m, ν):
     """
-    Determine a tuple of callables for a specific input of mass (m) and
-    symmetric mass ratio (ν).
+    Determine a tuple of callables for a specific input of mass (m)
+    and symmetric mass ratio (ν).
 
     Parameters
     ----------
@@ -20,35 +20,40 @@ def PN_charges(m, ν):
     angular_momentum_pn : callable
         Returns PN approximation for angular momentum up to 3PN order
     G_mag_pn : callable
-        Returns PN approximation for the magnitude of CoM charge up to leading
-        PN order
+        Returns PN approximation for the magnitude of CoM charge up to
+        leading PN order
     orbital_phase : callable
-        Returns the orbital phase from the (2,1) mode of the strain. See notes
-        for convention used.
+        Returns the orbital phase from the (2,1) mode of the strain.
+        See notes for convention used.
 
-    All returned callables accept an `ABD` object as an input parameter and
-    return their respective quantities evaluated at the same time steps as the
-    `ABD` object. The PN expressions for energy and angular momentum are taken
-    from Eqs. (337) and (338) of Blanchet's Living Review (2014)
-    <https://arxiv.org/abs/1310.1528>. The PN expression for CoM charge is
-    derived in Khairnar et al. <add_arXiv_link>. The tuple of these callables
-    can be passed as the `Gargsfun` keyword argument to
-    `map_to_superrest_frame`. These callables are then used to determine the
-    Gargs parameters to `transformation_from_CoM_charge`. They are called as
-    Gargs = [func(abd) for func in Gargsfun] if Gargsfun else None
-    within the `com_transformation_to_map_to_superrest_frame` function.
+    All returned callables accept an `ABD` object as an input
+    parameter and return their respective quantities evaluated at the
+    same time steps as the `ABD` object.  The PN expressions for
+    energy and angular momentum are taken from Eqs. (337) and (338) of
+    Blanchet's Living Review (2014) <https://arxiv.org/abs/1310.1528>.
+    The PN expression for CoM charge is derived in Khairnar et al.
+    <add_arXiv_link>.  The tuple of these callables can be passed as
+    the `Gargsfun` keyword argument to `map_to_superrest_frame`.
+    These callables are then used to determine the Gargs parameters to
+    `transformation_from_CoM_charge`.  They are called as
+    
+        Gargs = [func(abd) for func in Gargsfun] if Gargsfun else None
+        
+    within the `com_transformation_to_map_to_superrest_frame`
+    function.
 
     Notes
     ------
-    Our conventions for defining the orbital phase differ from the standard
-    conventions used in PN theory. This stems from the fact that SpEC uses
-    h_{ab} to define the metric perturbation while PN theory uses h^{ab} for
-    the metric perturbation, which results in h^{PN}_{l,m} = − h^{NR}_{l,m}. The
-    leading order PN expression for the h_{2,1} mode is (Eq. 492 of
-    <https://arxiv.org/abs/1310.1528>)
-    h^{PN}_{21} = (2 G ν m x)/R * \sqrt(16 \pi/5) * ((1/3) 𝒾 Δ x^{1/2} + O(x^{3/2})).
-    Because of the 𝒾 factor we get the orbital phase numerically as
-    ψ = - arg(-h^{NR}_{2,1}) + π/2.
+    Our conventions for defining the orbital phase differ from the
+    standard conventions used in PN theory.  This stems from the fact
+    that SpEC uses h_{ab} to define the metric perturbation while PN
+    theory uses h^{ab} for the metric perturbation, which results in
+    h^{PN}_{l,m} = − h^{NR}_{l,m}.  The leading order PN expression
+    for the h_{2,1} mode is (Eq. 492 of
+    <https://arxiv.org/abs/1310.1528>) h^{PN}_{21} = (2 G ν m x)/R *
+    \sqrt(16 \pi/5) * ((1/3) 𝒾 Δ x^{1/2} + O(x^{3/2})).  Because of
+    the 𝒾 factor we get the orbital phase numerically as ψ = -
+    arg(-h^{NR}_{2,1}) + π/2.
     """
 
     def compute_x(abd):
@@ -89,14 +94,15 @@ def PN_charges(m, ν):
 
 def analytical_CoM_func(θ, t, E, J_mag, G_mag, ψ):
     """
-    Compute a model time series for boosted center-of-mass (CoM) charge using PN
-    expressions of energy, angular momentum, and CoM charge, along with the
-    phase computed from the h_{21} mode.
+    Compute a model time series for boosted center-of-mass (CoM)
+    charge using PN expressions of energy, angular momentum, and CoM
+    charge, along with the phase computed from the h_{21} mode.
 
-    This model timeseries is derived in Khairnar et al. <add_arXiv_link>.
-    It serves as a fitting function that can be passed as the `Gfun` keyword
-    argument to the `map_to_superrest_frame` function. All the arguments are
-    computed over the window used for fixing the frame.
+    This model timeseries is derived in Khairnar et al.
+    <add_arXiv_link>.  It serves as a fitting function that can be
+    passed as the `Gfun` keyword argument to the
+    `map_to_superrest_frame` function.  All the arguments are computed
+    over the window used for fixing the frame.
 
     Parameters
     ----------
@@ -104,22 +110,25 @@ def analytical_CoM_func(θ, t, E, J_mag, G_mag, ψ):
         Parameters of the model.
         - θ[0:3] : components of the boost velocity
         - θ[3:6] : components of the spatial translation
-        - θ[6:]  : two additional fit parameters referred to as the nuisance
-                   parameters in Khairnar et al. <add_arXiv_link>.
+        - θ[6:]  : two additional fit parameters referred to as the
+                   nuisance parameters in Khairnar et al.
+                   <add_arXiv_link>.
     t: ndarray, real
-        Time array corresponding to the window over which the frame fixing is
-        performed.
+        Time array corresponding to the window over which the frame
+        fixing is performed.
     E: ndarray, real
-        PN approximation for the energy computed over the fitting window.
+        PN approximation for the energy computed over the fitting
+        window.
     J_mag: ndarray, real
-        PN approximation for the magnitude of angular momentum computed over the
-        fitting window.
+        PN approximation for the magnitude of angular momentum
+        computed over the fitting window.
     G_mag: ndarray, real
-        PN approximation for the magnitude of the CoM charge computed over the
-        fitting window.
+        PN approximation for the magnitude of the CoM charge computed
+        over the fitting window.
     ψ: ndarray, real
-        Unwrapped orbital phase obtained from the (2,1) mode of the strain over
-        the fitting window. See PN_charges for appropriate conventions.
+        Unwrapped orbital phase obtained from the (2,1) mode of the
+        strain over the fitting window.  See PN_charges for
+        appropriate conventions.
 
     Returns
     -------
